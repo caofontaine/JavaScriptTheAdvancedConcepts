386402ca8952f8b8102b8fd55488ae223855f9e4
caofontaine <benjamin.cao88@gmail.com>
2021-01-09 14:25:25 -0500

    add pure functions code 2

---
+++ S07/FP-2/FP-2.js
@@ -0,0 +1,24 @@
+//Side effects:
+const array = [1,2,3];
+function mutateArray(arr) {
+  arr.pop()
+}
+function mutateArray2(arr) {
+  arr.forEach(item => arr.push(1
+  ))
+}
+//The order of the function calls will matter.
+mutateArray(array)
+mutateArray2(array)
+array
+
+// map and concat methods can fix this issue of mutation
+function removeLastItem(arr) {
+  const.newArray = [].concat(arr);
+  newArray.pop();
+  return newArray;
+}
+
+function multiplyBy2(arr) {
+  return arr.map(item => item * 2);
+}
---
+++ S07/FP-2/README.md
@@ -0,0 +1,4 @@
+# Section 07: Functional Programming
+Lecture code on pure functions from lecture 114: Pure Functions 2
+
+[JavaScript: The Advanced Concepts by Andrei Neagoie](https://www.udemy.com/course/advanced-javascript-concepts/)
